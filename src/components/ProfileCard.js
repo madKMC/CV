5a832280fdf9d2555cf0c11fbfdd9acbda422572
Kaylen Cairns <kaylen.cairns@gmail.com>
2024-09-02 14:58:31 +0200

    Adjusted the image size for mobile devices.

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import profileImage from '../images/profile.jpg';
+import profileImageMobile from '../images/mobileProfile.jpg';
 
 const ProfileCard = () => {
     return (
         <Card fluid>
-            <Card.Img variant="top" src={profileImage} className="img-fluid" roundedCircle />
-            <Card.Body>
-              <Card.Title>Kaylen Cairns</Card.Title>
-              <Card.Text>
-                Aspiring Software Developer | BSc IT Student at NWU
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <picture>
+          <source media="(max-width: 767px)" srcSet={profileImageMobile} />
+            <source media="(min-width: 768px)" srcSet={profileImage} />
+            <Card.Img variant="top" className="img-fluid" roundedCircle />
+          </picture>
+          <Card.Body>
+            <Card.Title>Kaylen Cairns</Card.Title>
+            <Card.Text>
+              Aspiring Software Developer | BSc IT Student at NWU
+            </Card.Text>
+          </Card.Body>
+        </Card>
     )
 }
 
