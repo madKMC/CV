64e195ee203c2d2fa566a4c0a882f0c5f712b488
Kaylen Cairns <kaylen.cairns@gmail.com>
2025-09-05 13:25:41 +0200

    Adjusted the size of the profilecard to be smaller when on larger screens

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
 import profileImage from '../images/profile.jpg';
 
 const ProfileCard = () => {
 	return (
-		<Card fluid>
+		<Card className='profile-card'>
 			<Card.Img variant='top' src={profileImage} />
 			<Card.Body>
 				<Card.Title>Kaylen Cairns</Card.Title>
